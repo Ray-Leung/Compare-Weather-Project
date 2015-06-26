ec86acde5cab99ac68eb885887b1a2cf535c585e
WEILIN LIANG <rapetrust@gmail.com>
2015-06-26 17:59:16 +0800

    weather
    test

---
+++ dist/js/start.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2015/6/22.
+ */
+
+$.get("http://ipinfo.io", function (response) {
+    this.response = response;
+   // $("#ip").html("IP: " + this.response.ip);
+    $("#address").html("Location: " + this.response.city + ", " + this.response.region);
+   // $("#details").html(JSON.stringify(response, null, 4));
+}, "jsonp");
+
